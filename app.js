c6353eaa971c9c96313fb4b9bdb5b60a2ada7dbf
FlorenciaAmado <amadoflorencia8@gmail.com>
2022-11-24 10:13:31 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+
+// puerto y callback
+app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}`));
+//poner en el navegador localhost:3030
+//reiniciar servidor para que tome los cambios.
+
+
+
+
